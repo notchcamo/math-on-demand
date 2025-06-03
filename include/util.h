65b08ad0d08629f727c70ea18d735fc4c0a74780
notchcamo <notchcamo@gmail.com>
2025-06-03 12:03:48 +0900

    Modify util.h to shorten nested namespace declaration

--- include/util.h
+++ include/util.h
@@ -5,19 +5,17 @@
 
 #include <limits>
 
-namespace mathod {
+/**
+ * @brief Namespace for utility functions.
+ */
+namespace mathod::util {
     /**
-     * @brief Namespace for utility functions.
+     * @brief Returns true if the given value is nearly zero.
      */
-    namespace util {
-        /**
-         * @brief Returns true if the given value is nearly zero.
-         */
-        template <typename T>
-        bool is_zero(const T value, const T tolerance = std::numeric_limits<T>::epsilon()) {
-            return std::abs(value) < tolerance;
-        }
-    } // namespace util.
+    template <typename T>
+    bool is_zero(const T value, const T tolerance = std::numeric_limits<T>::epsilon()) {
+        return std::abs(value) < tolerance;
+    }
 } // namespace mathod.
 
 #endif //UTIL_H
