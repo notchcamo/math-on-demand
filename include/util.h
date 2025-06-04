7a59621f3c88c9b2a5f8f7029a0b5b88b80dba8e
notchcamo <notchcamo@gmail.com>
2025-06-04 15:58:21 +0900

    feat: Add util function isZero

--- include/util.h
+++ include/util.h
@@ -9,12 +9,22 @@
  */
 namespace mathod::util {
     /**
-     * @brief Returns true if the given values are nearly equal.
      * @tparam T Arithmetic types only.
+     * @return True if the given values are nearly equal.
      */
     template <typename T>
     requires std::is_arithmetic_v<T>
     bool isEqual(const T a, const T b, const T tolerance = std::numeric_limits<T>::epsilon()) {
         return std::abs(a - b) < tolerance;
+    }
+
+    /**
+     * @tparam T Arithmetic types only.
+     * @return True if the given value is nearly zero.
+     */
+    template <typename T>
+    requires std::is_arithmetic_v<T>
+    bool isZero(const T num, const T tolerance = std::numeric_limits<T>::epsilon()) {
+        return isEqual(num, static_cast<T>(0), tolerance);
     }
 } // namespace mathod.
