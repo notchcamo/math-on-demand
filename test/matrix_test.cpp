e67d9b59f8cb9a42222e6667e395730c4ce80fae
notchcamo <notchcamo@gmail.com>
2025-06-16 21:30:58 +0900

    test : Add Matrix inverse function test

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -169,3 +169,56 @@ TEST(Matrix, identity)
 
     EXPECT_EQ(mat1, mat1 * Matrix2L::createIdentity());
 }
+
+TEST(Matrix, inverse)
+{
+    // 2 x 2
+    const Matrix2L mat1{
+        {2, 1},
+        {1, 3}
+    };
+    const Matrix2L ans1{
+        {0.6, -0.2},
+        {-0.2, 0.4}
+    };
+
+    EXPECT_EQ(mat1.inverse2x2(), ans1);
+    EXPECT_EQ(mat1.inverseNxN(), ans1);
+    EXPECT_EQ(Matrix2L::createIdentity().inverse2x2(), Matrix2L::createIdentity());
+    EXPECT_EQ(Matrix2L::createIdentity().inverseNxN(), Matrix2L::createIdentity());
+
+    // 3 x 3
+    const Matrix3d mat2{
+        {1, 2, 3},
+        {0, 1, 4},
+        {5, 6, 0}
+    };
+    const Matrix3d ans2{
+        {-24, 18, 5},
+        {20, -15, -4},
+        {-5, 4, 1}
+    };
+
+    EXPECT_EQ(mat2.inverse3x3(), ans2);
+    EXPECT_EQ(mat2.inverseNxN(), ans2);
+    EXPECT_EQ(Matrix3d::createIdentity().inverse3x3(), Matrix3d::createIdentity());
+    EXPECT_EQ(Matrix3d::createIdentity().inverseNxN(), Matrix3d::createIdentity());
+
+    // 4 x 4
+    Matrix4L mat3 = Matrix4L::createIdentity();
+    EXPECT_EQ(mat3.inverseNxN(), mat3);
+
+    mat3 = {
+        {5, 6, 6, 8},
+        {2, 2, 2, 8},
+        {6, 6, 2, 8},
+        {2, 3, 6, 7}
+    };
+    const Matrix4L ans3{
+        {-17, -9, 12, 16},
+        {17, 8.75, -11.75, -16},
+        {-4, -2.25, 2.75, 4},
+        {1, 0.75, -0.75, -1}
+    };
+    EXPECT_EQ(mat3.inverseNxN(), ans3);
+}
