05d9fa71a5b81d1f49d308c7e5d96251da3276d8
notchcamo <notchcamo@gmail.com>
2025-06-16 21:29:42 +0900

    test : Add utility function test

---
+++ test/util_test.cpp
@@ -0,0 +1,19 @@
+// By notchcamo.
+
+#include "gtest/gtest.h"
+#include "mathod/mathod.h"
+
+using namespace mathod;
+
+TEST(Util, is_equal_range)
+{
+    const std::vector<int> vec1{1, 2, 3};
+    const std::vector<int> vec2{1, 2, 3};
+
+    EXPECT_TRUE(util::isEqual(vec1, vec2));
+
+    const std::array<double, 3> arr1{1.0, 2.0, 3.0};
+    const std::array<double, 3> arr2{1.0, 2.0, 3.0};
+
+    EXPECT_TRUE(util::isEqual(arr1, arr2));
+}
